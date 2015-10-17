1000e79a11c9531fba800111323d0deb2d400f99
miadea <kitcat2675@gmail.com>
2015-10-17 18:32:37 -0400

    updated for meteor 1.2

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'meteoric:ionicons-sass',
   summary: "Ionic's Ionicons library in SASS and bundled for Meteor.",
-  version: '0.1.7',
+  version: '0.2.0',
   git: 'https://github.com/meteoric/ionicons-sass'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use("fourseven:scss@3.2.0", ["server"]);
-  api.imply("fourseven:scss", ["server"]);
+  api.use("fourseven:scss");
+  api.imply("fourseven:scss");
 
-  api.addFiles([
+  api.addAssets([
     'fonts/ionicons.eot',
     'fonts/ionicons.svg',
     'fonts/ionicons.ttf',
@@ -22,7 +22,7 @@ Package.onUse(function(api) {
     'stylesheets/_ionicons-font.scss',
     'stylesheets/_ionicons-icons.scss',
     'stylesheets/_ionicons-variables.scss'
-  ], 'server');
+  ], 'client');
 
-  api.addFiles('_ionicons.scss', 'server');
+  api.addFiles('_ionicons.scss', 'client', {isImport: true});
 });
